5765884fa62bb4946ce3ebabb710ca85348ca5af
Cecchy2 <dariocecchinato@gmail.com>
2024-09-04 16:32:57 +0200

    aggiunti test e asserzioni

--- src/test/java/dariocecchinato/s17l3_spring_tests_project/S17l3SpringTestsProjectApplicationTests.java
+++ src/test/java/dariocecchinato/s17l3_spring_tests_project/S17l3SpringTestsProjectApplicationTests.java
@@ -4,14 +4,17 @@ import dariocecchinato.s17l3_spring_tests_project.Enum.Stato;
 import dariocecchinato.s17l3_spring_tests_project.Enum.StatoOrdine;
 import dariocecchinato.s17l3_spring_tests_project.entities.MenuData;
 import dariocecchinato.s17l3_spring_tests_project.entities.Ordine;
+import dariocecchinato.s17l3_spring_tests_project.entities.Pizza;
 import dariocecchinato.s17l3_spring_tests_project.entities.Tavolo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class S17l3SpringTestsProjectApplicationTests {
@@ -33,4 +36,33 @@ class S17l3SpringTestsProjectApplicationTests {
 		assertEquals(tavolo1, ordine.getTavolo());
 	}
 
+	@Test
+	public void testCalcolaTotale() {
+
+		Tavolo tavolo = new Tavolo(1, 4, Stato.OCCUPATO);
+		List<MenuData>datas = new ArrayList<>();
+		MenuData pizza = new Pizza("Margherita", 4.99, 1104);
+		datas.add(pizza);
+		Ordine ordine = new Ordine(tavolo, datas, 1, StatoOrdine.IN_CORSO, 2);
+		ordine.setCostoCoperto(2.5);
+
+		double expectedTotal = 2 * 2.5 + 4.99;
+		assertEquals(expectedTotal, ordine.calcolaTotale());
+		assertTrue(ordine.calcolaTotale()>0);
+		assertNotNull(ordine.getOraAcquisizione());
+	}
+
+	@ParameterizedTest
+	@CsvSource({"testPizza, 7.00,700", "testPizza2, 8.00, 800"})
+	void testNewPizzaMaker(String nome,double prezzo,int nutritionalInfo){
+		Pizza pizza = new Pizza(nome,prezzo,nutritionalInfo);
+		assertNotNull(pizza);
+		assertTrue(pizza.getPrezzo()>0);
+		assertFalse(pizza.getValoriNutrizionali()<0);
+		assertAll(
+				()->assertEquals(nome, pizza.getNome()),
+				()->assertTrue(pizza.getPrezzo()>0),
+				()->assertEquals(nutritionalInfo, pizza.getValoriNutrizionali())
+		);
+	}
 }
