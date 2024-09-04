1eef73625d944029e2b78d66f104ba3a9c264d27
Cecchy2 <dariocecchinato@gmail.com>
2024-09-04 17:50:15 +0200

    aggiunto test con context

--- src/test/java/dariocecchinato/s17l3_spring_tests_project/S17l3SpringTestsProjectApplicationTests.java
+++ src/test/java/dariocecchinato/s17l3_spring_tests_project/S17l3SpringTestsProjectApplicationTests.java
@@ -3,6 +3,7 @@ package dariocecchinato.s17l3_spring_tests_project;
 import dariocecchinato.s17l3_spring_tests_project.Enum.Stato;
 import dariocecchinato.s17l3_spring_tests_project.Enum.StatoOrdine;
 import dariocecchinato.s17l3_spring_tests_project.entities.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -18,22 +19,48 @@ import static org.junit.jupiter.api.Assertions.*;
 class S17l3SpringTestsProjectApplicationTests {
 
 	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(S17l3SpringTestsProjectApplication.class);
-	
+
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	void testOrdineConstructor(){
-		Tavolo tavolo1= new Tavolo(1, 6, Stato.OCCUPATO);
-		List<MenuData> ordineList = new ArrayList<>();
+	void testPizzaAddingMenu (){
+		Menu menu= context.getBean(Menu.class);
+		Topping onions = (Topping) context.getBean("onions");
+		Pizza pizzaTest = new Pizza("pizzaTest",5.00,200);
+		menu.addData(pizzaTest);
+		pizzaTest.addTopping(onions);
+		assertEquals(pizzaTest,menu.getDatas().getLast());
+		assertNotNull(menu);
+		assertTrue(pizzaTest.getToppings().contains(onions));
 
-		Ordine ordine = new Ordine(tavolo1,ordineList,1, StatoOrdine.IN_CORSO,5);
+	}
 
-		assertEquals(1,ordine.getNumeroOrdine());
-		assertEquals(StatoOrdine.IN_CORSO,ordine.getStatoOrdine());
-		assertEquals(5,ordine.getNumeroCoperti());
-		assertEquals(tavolo1, ordine.getTavolo());
+	@Test
+	void pizzaContent(){
+		Pizza salamiPizza = (Pizza) context.getBean("salamiPizza");
+		assertNotNull(salamiPizza);
+		List<Topping> toppings = salamiPizza.getToppings();
+
+		assertTrue(toppings.contains(context.getBean("tomato")));
+		assertTrue(toppings.contains(context.getBean("cheese")));
+		assertTrue(toppings.contains(context.getBean("salami")));
+		assertEquals(3, toppings.size());
+	}
+
+	@ParameterizedTest
+	@CsvSource({"testPizza, 7.00,700", "testPizza2, 8.00, 800"})
+	void testNewPizzaMaker(String nome,double prezzo,int nutritionalInfo){
+		Pizza pizza = new Pizza(nome,prezzo,nutritionalInfo);
+		assertNotNull(pizza);
+		assertTrue(pizza.getPrezzo()>0);
+		assertFalse(pizza.getValoriNutrizionali()<0);
+		assertAll(
+				()->assertEquals(nome, pizza.getNome()),
+				()->assertTrue(pizza.getPrezzo()>0),
+				()->assertEquals(nutritionalInfo, pizza.getValoriNutrizionali())
+		);
 	}
 
 	@Test
@@ -51,43 +78,5 @@ class S17l3SpringTestsProjectApplicationTests {
 		assertTrue(ordine.calcolaTotale()>0);
 		assertNotNull(ordine.getOraAcquisizione());
 	}
-
-	@ParameterizedTest
-	@CsvSource({"testPizza, 7.00,700", "testPizza2, 8.00, 800"})
-	void testNewPizzaMaker(String nome,double prezzo,int nutritionalInfo){
-		Pizza pizza = new Pizza(nome,prezzo,nutritionalInfo);
-		assertNotNull(pizza);
-		assertTrue(pizza.getPrezzo()>0);
-		assertFalse(pizza.getValoriNutrizionali()<0);
-		assertAll(
-				()->assertEquals(nome, pizza.getNome()),
-				()->assertTrue(pizza.getPrezzo()>0),
-				()->assertEquals(nutritionalInfo, pizza.getValoriNutrizionali())
-		);
-	}
-	@Test
-	void testContext (){
-		Menu menu= context.getBean(Menu.class);
-		Pizza pizzaTest = new Pizza("pizzaTest",5.00,200);
-		menu.addData(pizzaTest);
-		assertNotNull(menu);
-		assertEquals(pizzaTest,menu.getDatas().getLast());
-	}
-
-	@Test
-	void pizzaContent(){
-
-		Pizza salamiPizza = (Pizza) context.getBean("salamiPizza");
-		assertNotNull(salamiPizza);
-		List<Topping> toppings = salamiPizza.getToppings();
-
-		assertTrue(toppings.contains(context.getBean("tomato")));
-		assertTrue(toppings.contains(context.getBean("cheese")));
-		assertTrue(toppings.contains(context.getBean("salami")));
-		assertEquals(3, toppings.size());
-	}
-
-
-
 
 }
